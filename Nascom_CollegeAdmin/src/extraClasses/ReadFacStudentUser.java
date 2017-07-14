6a1745f98692e26ff8c2c03291cf070d39898c7b
Ecos24 <divyang1996@gmail.com>
2017-07-14 15:26:28 +0530

    com

--- Nascom_CollegeAdmin/src/extraClasses/ReadFacStudentUser.java
+++ Nascom_CollegeAdmin/src/extraClasses/ReadFacStudentUser.java
@@ -98,6 +98,91 @@ public class ReadFacStudentUser
 	public static FacultyUser readFacUser(String facid)
 			throws ClassNotFoundException, SQLException
 	{
-		return null;
+		FacultyUser user = new FacultyUser();
+		Connection con = GetDBConnection.getConnection();
+		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM "+ReadProjectProperties.getProp("TNFACPERSONAL")+" WHERE facid = '"+facid+"';");
+		while( rs.next() )
+		{
+			user.setfName(rs.getString("fname"));
+			user.setmName(rs.getString("mname"));
+			user.setlName(rs.getString("lname"));
+			user.setDob(rs.getDate("dob"));
+			user.setGender(rs.getString("gender"));
+			user.setBranch(rs.getString("branch"));
+			user.seteMail(rs.getString("email"));
+			user.setMobNo(rs.getLong("mobno"));
+			user.setAadhar(rs.getLong("aadhar"));
+			user.setImgPath(rs.getString("imagepath"));
+			int addid = rs.getInt("addid");
+			@SuppressWarnings("unused")
+			int corespaddid = rs.getInt("corespaddid");
+			user.setClsXRollNo(rs.getString("clsxrollno"));
+			user.setClsXIIRollNo(Long.parseLong(rs.getString("clsxiirollno")));
+			int gid = rs.getInt("guardianid");
+			int higheduid = rs.getInt("highestqualid");
+			user.setExperienceYr(rs.getInt("experienceyr"));
+			user.setLastTaught(rs.getString("lasttaught"));
+			int subid = rs.getInt("subid");
+			user.setNameOfPersuingCourse(rs.getString("persuingcourse"));
+			Statement st1 = con.createStatement();
+			ResultSet rs1 = st1.executeQuery("SELECT * FROM "+ReadProjectProperties.getProp("TNADDRESS")+" WHERE addid = '"+addid+"';");
+			while( rs1.next() )
+			{
+				user.setAdd(rs1.getString("address"));
+				user.setCity(rs1.getString("city"));
+				user.setState(rs1.getString("state"));
+				user.setPinCode(rs1.getInt("pincode"));
+				user.setCorespAdd(rs1.getString("address"));
+				user.setCorespCity(rs1.getString("city"));
+				user.setCorespState(rs1.getString("state"));
+				user.setCorespPinCode(rs1.getInt("pincode"));
+			}
+			
+			Statement st2 = con.createStatement();
+			ResultSet rs2 = st2.executeQuery("SELECT * FROM "+ReadProjectProperties.getProp("TNGUARDIAN")+" WHERE gid = '"+gid+"';");
+			while( rs2.next() )
+			{
+				user.setGuardianName(rs2.getString("name"));
+				user.setGuardianMobNo(rs2.getLong("mobno"));
+				user.setGuardianEMail(rs2.getString("email"));
+				user.setGuardianOccupation(rs2.getString("occupation"));
+			}
+			Statement st3 = con.createStatement();
+			ResultSet rs3 = st3.executeQuery("SELECT * FROM "+ReadProjectProperties.getProp("TNFACHIGHEREDU")+" WHERE gid = '"+higheduid+"';");
+			while( rs3.next() )
+			{
+				user.setHighestQual(rs3.getString("highqual"));
+				user.setHighestQualFrom(rs3.getString("highqualfrom"));
+				user.setHighestQualPassingYr(rs3.getInt("passingyr"));
+				user.setHighestQualAggregate(rs3.getFloat("aggregate"));
+			}
+			Statement st4 = con.createStatement();
+			ResultSet rs4 = st4.executeQuery("SELECT * FROM "+ReadProjectProperties.getProp("TNEDUCATION")+" WHERE rollno = '"+user.getClsXRollNo()+"';");
+			while( rs4.next() )
+			{
+				user.setClsXBoard(rs4.getString("board"));
+				user.setClsXPassingYr(rs4.getInt("passingyr"));
+			}
+			Statement st5 = con.createStatement();
+			ResultSet rs5 = st5.executeQuery("SELECT * FROM "+ReadProjectProperties.getProp("TNEDUCATION")+" WHERE rollno = '"+user.getClsXIIRollNo()+"';");
+			while( rs5.next() )
+			{
+				user.setClsXIIBoard(rs5.getString("board"));
+				user.setClsXIIMedium(rs5.getString("medium"));
+				user.setClsXIIPassingYr(rs5.getInt("passingyr"));
+				user.setClsXIIPercentage(rs5.getFloat("percentage"));
+			}
+			Statement st6 = con.createStatement();
+			ResultSet rs6 = st6.executeQuery("SELECT * FROM "+ReadProjectProperties.getProp("TNFACSUB")+" WHERE facsubid = '"+subid+"';");
+			while( rs6.next() )
+			{
+				user.setSubject1(rs6.getString("sub1"));
+				user.setSubject2(rs6.getString("sub2"));
+				user.setSubject3(rs6.getString("sub3"));
+			}
+		}
+		return user;
 	}
 }
